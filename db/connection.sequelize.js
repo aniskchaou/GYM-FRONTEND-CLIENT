c1acaf3f689179a79f030d58e35e492c1a244164
Anis KCHAOU <kchaouanis26@gmail.com>
2022-08-26 17:31:50 +0200

    new changes

--- db/connection.sequelize.js
+++ db/connection.sequelize.js
@@ -1,10 +1,25 @@
 
 
-module.exports = {
+/* module.exports = {
     HOST: "localhost",
     USER: "postgres",
     PASSWORD: "admin",
     DB: "gym",
+    dialect: "postgres",
+    port: 5432,
+    native: false,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+}; */
+module.exports = {
+    HOST: "ec2-34-236-215-156.compute-1.amazonaws.com",
+    USER: "chbyiyvpixlzez",
+    PASSWORD: "5b961ee894b0d83e8254d99a023484edb893aac3ec1d10f1bbdb67d0d5e69713",
+    DB: "d44nd3kpidbi5r",
     dialect: "postgres",
     port: 5432,
     native: false,
--- db/db.js
+++ db/db.js
@@ -1,13 +1,19 @@
 const { Client } = require('pg');
 
-const client = new Client({
+/* const client = new Client({
     user: 'postgres',
     host: 'localhost',
     database: 'gym',
     password: 'admin',
     port: 5432,
+}); */
+const client = new Client({
+    user: 'chbyiyvpixlzez',
+    host: 'ec2-34-236-215-156.compute-1.amazonaws.com',
+    database: 'd44nd3kpidbi5r',
+    password: '5b961ee894b0d83e8254d99a023484edb893aac3ec1d10f1bbdb67d0d5e69713',
+    port: 5432,
 });
-
 /*
 client.connect();
 
@@ -43,4 +49,4 @@ function initDb()
         });
 }    
 */
-module.exports=client;    
+module.exports = client;    
