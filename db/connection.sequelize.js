db7b91a0efba72572ac4fd92d7e338a49063339c
Anis KCHAOU <kchaouanis25@gmail.com>
2022-10-13 17:01:50 +0200

    Update connection.sequelize.js

--- db/connection.sequelize.js
+++ db/connection.sequelize.js
@@ -3,11 +3,11 @@
 module.exports = {
     HOST: "ec2-52-4-87-74.compute-1.amazonaws.com",
     USER: "ejfxcyzjjxneud",
-    PASSWORD: "ef0cf8ac6f12f6ed4ead19e35a6f1f88a3e3e1e4e60d213f30e035ba9e538d4",
+    PASSWORD: "ef0cf8ac6f12f6ed4ead19e35a6f1f88a3e3e1e4e60d213f30e035ba9e538d4d",
     DB: "dcs6tuuaplp5pd",
     dialect: "postgres",
     port: 5432,
-    native: false,
+    native: true,
     pool: {
         max: 5,
         min: 0,
